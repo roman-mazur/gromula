6858ae8adcb8620df9189888b0faa2ea85ed963e
mazur.roman <mazur.roman@09aaff22-18cd-11de-9433-ad2e3fad96c5>
2009-04-28 16:36:13 +0000

    new additions

---
+++ src/main/groovy/org/mazur/gromula/Images.groovy
@@ -0,0 +1,26 @@
+package org.mazur.gromula
+
+import javax.swing.Iconimport javax.swing.ImageIcon
+
+/**
+ * Images.
+ * Version: $Id$
+ *
+ * @author Roman Mazur (mailto: mazur.roman@gmail.com)
+ *
+ */
+public class Images {
+
+  /** Images map. */
+  private def imagesMap = [:]
+  
+  public Icon get(def name) {
+    Icon result = imagesMap[name]
+    if (result) { return result }
+    def resource = Images.class.getResource('/buttons/' + name.toLowerCase() + '.ico')
+    result = new ImageIcon(resource)
+    imagesMap[name] = result
+    return result
+  }
+  
+}
